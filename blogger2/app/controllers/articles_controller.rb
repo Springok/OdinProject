442798f35d3333c9722f4c56a6c527ed12f55eeb
Springok <wenchuan.lin.tw@gmail.com>
2015-11-23 11:40:14 +0800

    form-based workflow feature completed

--- blogger2/app/controllers/articles_controller.rb
+++ blogger2/app/controllers/articles_controller.rb
@@ -9,4 +9,41 @@ def show
 
 end
 
+def new
+  @article = Article.new
+end
+
+def create
+  @article = Article.new(article_params)
+  @article.save
+
+  redirect_to article_path(@article)
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to articles_path
+
+  flash[:notice] = "The article '#{@article.title}' has been deleted"
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+
+  @article.update(article_params)
+
+  redirect_to article_path(@article)
+  flash[:notice] = "The article '#{@article.title}' has been update"
+end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
