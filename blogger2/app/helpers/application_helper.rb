29f891a59c9d0512310d595e3c059bb7b6e609b6
Springok <wenchuan.lin.tw@gmail.com>
2015-11-23 14:05:55 +0800

    Finished blog comments feature

--- blogger2/app/helpers/application_helper.rb
+++ blogger2/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+def render_article_comments_count(article)
+  article.comments.size
+end
+
+def render_article_comment_created_at(comment)
+  distance_of_time_in_words(comment.article.created_at, comment.created_at)
+end
+
 end
