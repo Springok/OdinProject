6cca77426fab7e8253460c9cca2d0bf1e62fee43
Springok <wenchuan.lin.tw@gmail.com>
2015-11-24 18:55:28 +0800

    Authentication use Sorcery gem

--- blogger2/config/routes.rb
+++ blogger2/config/routes.rb
@@ -19,6 +19,14 @@ Rails.application.routes.draw do
     resources :tags
     root 'articles#index'
 
+    resources :authors
+
+    resources :author_sessions, only: [ :new, :create, :destroy ]
+
+    get 'login'  => 'author_sessions#new'
+    get 'logout' => 'author_sessions#destroy'
+
+
   # Example resource route with options:
   #   resources :products do
   #     member do
